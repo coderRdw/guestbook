4c6106916a1ec77b8a36b0e387dfb7b9748c91c9
coderRdw <1846019737@qq.com>
2021-09-12 17:07:45 +0800

    第二次提交

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const fs = require('fs')
 const http = require('http')
 const template = require('art-template')
+const url = require('url')
 // 1.将所有的html页面都放在views中
 // 2.为了方便请求资源，将这些静态资源放在public目录中
 // 3.public目录中所有资源都可以被访问
@@ -37,8 +38,13 @@ const comments = [
 ]
 // 链式调用格式
 http.createServer((req, res) => {
-    const url = req.url
-  if (url === '/') {
+  // 使用url.parse方法将路径解析为一个方便操作的!对象!，第二个参数为true
+  let parseObj = url.parse(req.url, true)
+  // true属性将字符串转为对象，便于拿数据
+  // pathName是指url中不包含查询字符串的路径部分（不包括？之后的部分）
+  let pathName = parseObj.pathname
+
+  if (pathName === '/') {
     fs.readFile('./views/index.html', (err, data) => {
       if (err) {
         return res.end('404')
@@ -48,7 +54,25 @@ http.createServer((req, res) => {
       })
       res.end(content)
     })
-  } else if (url === '/post') {
+  } else if (pathName === '/comments') {
+    // !获取/comments?name=xxx?
+    // 1.获取表单数据
+    // 2.生成日期
+    // 3.重定向至首页
+    let comment = parseObj.query
+    comment.dateTime = '2021-09-12'
+    comments.unshift(comment)
+    // console.log(comments)
+    // 如何通过服务器让客户端重定向？
+    // 1.状态码设置为302临时重定向 301永久重定向
+    // statusCode
+    // 2.在响应头重通过location告诉客户端往哪重定向
+    // setHeader
+    // 若客户端收到服务器的状态码是302，就会自动去响应头重查找location,从而得以重定向
+    res.statusCode = 302
+    res.setHeader('Location','/')
+    res.end()
+  } else if (pathName === '/post') {
     fs.readFile('./views/post.html', (err, data) => {
       if (err) {
         return res.end('404')
@@ -56,8 +80,8 @@ http.createServer((req, res) => {
       res.end(data)
     })
     // !这里使用indexOf方法是为了匹配url
-  } else if (url.indexOf('/public/') === 0) {
-    fs.readFile('.' + url, (err, data) => {
+  } else if (pathName.indexOf('/public/') === 0) {
+    fs.readFile('.' + pathName, (err, data) => {
       if (err) {
         return res.end('404')
       } 
@@ -74,3 +98,5 @@ http.createServer((req, res) => {
 }).listen(3000, () => {
   console.log('serve is running')
 })
+
+// 
